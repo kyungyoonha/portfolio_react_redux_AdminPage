5303764a41b914d68935749e9ce16328fc9b4562
Kyungyoon Ha <gkb10a@naver.com>
2020-09-09 12:28:17 +0900

    Reservation Chart Monthly / Weekly

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,52 +1,15 @@
 import React from "react";
 import styled from "styled-components";
-import GoogleMap from "../components/GoogleMap/GoogleMap";
 import CardContainer from "../components/common/CardContainer";
 import ChartsRegion from "../components/ChartsRegion";
+import ChartsWeeklyReservation from "../components/ChartsWeeklyReservation";
+import ChartsMonthlyReservation from "../components/ChartsMonthlyReservation";
 
 const Container = styled.div`
     background: #eeeeee;
     flex: 1;
     padding: 15px;
 `;
-
-const StyledContent = styled.div`
-    margin: 24px;
-    flex: 1;
-`;
-
-const data = [
-    {
-        id: "대한민국",
-        label: "대한민국",
-        value: 443,
-        color: "hsl(348, 70%, 50%)",
-    },
-    {
-        id: "태국",
-        label: "태국",
-        value: 477,
-        color: "hsl(248, 70%, 50%)",
-    },
-    {
-        id: "베트남",
-        label: "베트남",
-        value: 570,
-        color: "hsl(178, 70%, 50%)",
-    },
-    {
-        id: "이집트",
-        label: "이집트",
-        value: 159,
-        color: "hsl(142, 70%, 50%)",
-    },
-    {
-        id: "미안마",
-        label: "미안마",
-        value: 270,
-        color: "hsl(249, 70%, 50%)",
-    },
-];
 
 const Dashboard = () => {
     return (
@@ -59,16 +22,20 @@ const Dashboard = () => {
             <CardContainer>
                 <div className="card-header bg-white">지역별 그래프</div>
                 <div className="card-body bg-white">
-                    <ChartsRegion data={data} />
+                    <ChartsRegion />
                 </div>
             </CardContainer>
             <CardContainer>
                 <div className="card-header bg-white">월간 예약건</div>
-                <div className="card-body bg-white">Content</div>
+                <div className="card-body bg-white">
+                    <ChartsMonthlyReservation />
+                </div>
             </CardContainer>
             <CardContainer>
                 <div className="card-header bg-white">주간 예약건</div>
-                <div className="card-body bg-white">Content</div>
+                <div className="card-body bg-white">
+                    <ChartsWeeklyReservation />
+                </div>
             </CardContainer>
         </Container>
     );
